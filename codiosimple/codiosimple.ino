7883013127c6caf8674ec0c78ae7f8b0192d33d7
Kidd <cosuxx@xuqueralumnat.es>
2023-10-16 22:16:23 +0200

    Initial commit

---
+++ codiosimple/codiosimple.ino
@@ -0,0 +1,33 @@
+int IN1 = 6;
+int IN2 = 7;
+int enA = 3;
+//int IN3 = 11;
+//int IN4 = 10;
+
+
+
+
+void setup() {
+
+
+ pinMode (IN1, OUTPUT);
+ pinMode (IN2, OUTPUT);
+ pinMode (enA, OUTPUT);
+ //pinMode (IN4, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite (IN1, HIGH);
+  digitalWrite (IN2, HIGH);
+  //digitalWrite (IN3, HIGH);
+  //digitalWrite (IN4, HIGH);
+  //delay(2000);
+  //digitalWrite (IN1, LOW);
+  //digitalWrite (IN2, LOW);
+  //delay(2000);
+  //digitalWrite (IN3, HIGH);
+  //digitalWrite (IN4, HIGH);
+    
+
+}
